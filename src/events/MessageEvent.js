bee9b1eff790c4082eb82ba74e98330fd8415cb5
Itta <86896657+itta611@users.noreply.github.com>
2022-02-04 17:33:42 +0900

    remove function/adapter

--- src/events/MessageEvent.js
+++ src/events/MessageEvent.js
@@ -15,7 +15,6 @@ class MessageEvent {
     this.client.adapter.get(`${Routes.API.user(this.client.username)}messages/count/`).then(res => {
       if (res.data.count > 0) {
         this.client.emit("message", res.data.count); // TODO: Add message data
-        this.client.adapter.post('https://scratch.mit.edu/site-api/messages/messages-clear/');
       }
     });
   }

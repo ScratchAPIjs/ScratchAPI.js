d4f1b67d5579ffd857418cd1634f5c9d83666aba
Itta <86896657+itta611@users.noreply.github.com>
2022-02-04 17:58:59 +0900

    only callback when receve new message

--- src/events/MessageEvent.js
+++ src/events/MessageEvent.js
@@ -4,18 +4,24 @@ const { Routes } = require("../session/Addresses");
 class MessageEvent {
   constructor(client) {
     this.client = client;
+    this.messageCount = 0;
   }
 
-  start() {
+  async start() {
+    await this.client.adapter.get(`${Routes.API.user(this.client.username)}messages/count/`).then(res => {
+      this.messageCount = res.data.count;
+    });
+
     this.eventUpdate();
     setInterval(this.eventUpdate.bind(this), Constants.API.MESSAGE_EVENT_FREQUENCE);
   }
 
   eventUpdate() {
     this.client.adapter.get(`${Routes.API.user(this.client.username)}messages/count/`).then(res => {
-      if (res.data.count > 0) {
+      if (this.messageCount > res.data.count) {
         this.client.emit("message", res.data.count); // TODO: Add message data
       }
+      this.messageCount = res.data.count;
     });
   }
 }

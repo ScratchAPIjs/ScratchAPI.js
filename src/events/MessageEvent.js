ea90c825f0a1af0500df3054e364c939ebd0a8f5
Itta <86896657+itta611@users.noreply.github.com>
2022-02-04 16:46:35 +0900

    use router

--- src/events/MessageEvent.js
+++ src/events/MessageEvent.js
@@ -1,4 +1,5 @@
 const { Constants } = require("../configs/Constants");
+const { Routes } = require("../session/Addresses");
 
 class MessageEvent {
   constructor(client) {
@@ -7,11 +8,11 @@ class MessageEvent {
 
   start() {
     this.eventUpdate();
-    setInterval(this.eventUpdate.bind(this), Constants.API.MESSAGE_EVENT_FREQUENCE)
+    setInterval(this.eventUpdate.bind(this), Constants.API.MESSAGE_EVENT_FREQUENCE);
   }
 
   eventUpdate() {
-    this.client.adapter.get(`https://api.scratch.mit.edu/users/${this.client.username}/messages/count`).then(res => {
+    this.client.adapter.get(`${Routes.API.user(this.client.username)}messages/count/`).then(res => {
       if (res.data.count > 0) {
         this.client.emit("message", res.data.count); // TODO: Add message data
         this.client.adapter.post('https://scratch.mit.edu/site-api/messages/messages-clear/');
